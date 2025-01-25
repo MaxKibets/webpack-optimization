aba226d78106b72bbe046245327002a2ca3676b6
MaxKibets <kibetss3@gmail.com>
2025-01-25 15:16:56 +0200

    webpack-dev-server setting up

--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -1,8 +1,24 @@
 const commonConfig = require("./webpack.common.config.js");
 const { merge } = require("webpack-merge");
+const path = require("path");
 
 const config = merge(commonConfig, {
   mode: "development",
+  // webpack-dev-server plugin configuration
+  devServer: {
+    port: 9000,
+    static: {
+      directory: path.resolve(__dirname, ".."),
+    },
+    devMiddleware: {
+      index: "index.html",
+      writeToDisk: true,
+    },
+    client: {
+      overlay: true,
+    },
+    liveReload: false,
+  },
 });
 
 module.exports = config;
